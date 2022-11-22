6528abc1e2f9d17ef7200a996cd5b641a3f525a4
QunFan <allenfan1012@gmail.com>
2022-11-22 14:34:06 +1100

    commit tree ee6c7d13d4525428b66e4dd3850b4c554ef77e8c

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_names)# code an argument here
   # Your code here
+  dwarf_names.map.with_index(1) do |name,index|
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  
+  planeteer_calls.map {|call|"#{call}!"}
+end
+   
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  if array.size<4
+    false
+  else true
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.includes?("cheddar") || array.includes?("gouda") || array.includes?("camembert")
+    array.find {|cheese| cheese="camembert" || cheese="gouda" || cheese="cheddar"}
+  else nil
+  end
 end
